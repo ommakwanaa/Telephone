ebc3ee5d737f147d2dfc8b838051aa6a7442f99d
nirmal56 <nirmal.patel@einfochips.com>
2022-04-17 21:34:40 +0530

    insertion done with sorting

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 
 struct node
 {
-    long phoneNumber;
+    long unsigned int phoneNumber;
     char name[20];
     struct node *next;
 };
 
-struct node *insAtBeg(char *x, int number, struct node *first)
+struct node *insAtBeg(char *x, long unsigned int number, struct node *first)
 {
     struct node *new;
     new = (struct node *)malloc(sizeof(struct node));
@@ -28,9 +28,42 @@ struct node *insAtBeg(char *x, int number, struct node *first)
         return new;
     }
 }
+struct node* insOrd(char *x, long unsigned int number, struct node *first){    //inserts in ordered linked list in ordered manner
+	
+    struct node *new;
+	new = (struct node*)malloc(sizeof(struct node));
+	
+    struct node *save;
+	save = first;
+	
+	if(save == NULL){
+		printf("empty\n");
+		return new;
+	}
+	
+    for (int i = 0; i < 20; i++)
+        new->name[i] = x[i];
+    new->phoneNumber = number;
+	
+	if(new->name[0] <= first->name[0]){
+		new->next=first;
+		return new;
+	}
+	
+	while(save->next != NULL && new->name[0] >= save->next->name[0]){
+		save=save->next;
+	}
+	
+	new->next =save->next;
+	save->next=new;
+	return first;
+}
 
-void display(){
-
+void display(struct node *first){
+    while(first != NULL){
+        printf("\n\n%s\t%ld\n\n",first->name,first->phoneNumber);
+        first = first -> next;
+    }
 }
 int search(char *x, struct node *first)
 {
@@ -81,7 +114,8 @@ int main()
             scanf("%s", name);
             printf("\nenter number:");
             scanf("%d", &number);
-                first = insAtBeg(name, number, first);
+                // first = insAtBeg(name, number, first);
+            first = insOrd(name,number,first);
 
             break;
         case 2:
@@ -91,10 +125,11 @@ int main()
             printf("%d", search(name, first));
             break;
         case 3:
-            printf("read");
+            printf("\n\nreading\n\n");
+            display(first);
             break;
         case 4:
-            printf("short");
+            printf("sort");
             break;
         case 5:
             printf("delete");
